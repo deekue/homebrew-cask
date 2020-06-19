88adb3b1198db6b62bd43fa981814ffc1b4e05b0
DQ <daniel@chaosengine.net>
2020-06-18 19:32:26 -0700

    add support for extra files in Alacritty
    in response to https://github.com/alacritty/alacritty/issues/2653#issuecomment-595623118
    requires https://github.com/alacritty/alacritty/pull/3441

--- Casks/alacritty.rb
+++ Casks/alacritty.rb
@@ -9,8 +9,28 @@ cask 'alacritty' do
 
   app 'Alacritty.app'
   binary "#{appdir}/Alacritty.app/Contents/MacOS/alacritty"
-
+  
+  postflight do
+    system_command 'gzip -c #{appdir}/Alacritty.app/Contents/Resources/alacritty.man > #{HOMEBREW_PREFIX}/share/man/man1/alacritty.1.gz'
+    system_command 'tic -xe alacritty,alacritty-direct #{appdir}/Alacritty.app/Contents/Resources/alacritty.info'
+    FileUtils.mv('#{appdir}/Alacirtty.app/Contents/Resources/completitions/alacritty.bash', '#{HOMEBREW_PREFIX}/etc/bash_completion.d/alacritty')
+    FileUtils.mv('#{appdir}/Alacirtty.app/Contents/Resources/completitions/_alacritty', '#{HOMEBREW_PREFIX}/share/zsh/site-functions/_alacritty')
+    FileUtils.mv('#{appdir}/Alacirtty.app/Contents/Resources/completitions/alacritty.fish', '#{HOMEBREW_PREFIX}#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/alacritty')
+  end
+  
+  uninstall delete: [
+    '#{HOMEBREW_PREFIX}/share/man/man1/alacritty.1.gz',
+    '#{HOMEBREW_PREFIX}/etc/bash_completion.d/alacritty',
+    '#{HOMEBREW_PREFIX}/share/zsh/site-functions/_alacritty',
+    '#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/alacritty',    
+                    ]
+  
   zap delete: [
                 '~/Library/Saved Application State/io.alacritty.savedState',
+                '~/.terminfo/61/alacritty',
               ]
+  
+  caveats do
+    files_in_usr_local
+  end
 end
